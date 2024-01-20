3ce265f0db3543b7df45c33e48f56470ec7d09e1
Renzaki <renzakithakuri@gmail.com>
2024-01-20 16:29:45 +0545

    creating user

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,78 @@
-const { error } = require("console");
-const express = require("express");
-const mongoose = require("mongoose");
-const multer = require("multer");
-const path = require("path");
-require("dotenv").config();
+// const { error } = require("console");
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const multer = require("multer");
+// const path = require("path");
+
+import mongoose from "mongoose";
+import multer from "multer";
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import { Product } from "./models/Product.model.js";
+import bodyParser from "body-parser";
+
+dotenv.config({
+  path: "./.env",
+});
+// require("dotenv").config();
 
 const app = express();
 
-mongoose.connect(
-  `${process.env.MONGODB}/e-comm`,
-);
+mongoose.connect(`${process.env.MONGODB}/e-comm`);
 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Express app running");
 });
 
-const storage =  multer.diskStorage({
-    destination:'./upload/images',
-    filename:(req,file,cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
 
-})
+const upload = multer({ storage: storage });
 
-const upload = multer({storage:storage})
+app.use("/images", express.static("upload/images"));
 
-app.use('/images',express.static('upload/images'))
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${process.env.PORT}/images/${req.file.filename}`,
+  });
+});
 
+app.post("/addproduct", async (req, res) => {
+  const { id, name, image, old_price, new_price, category } = req.body;
 
-app.post('/upload',upload.single('product'),(req,res)=>{
-    res.json({
-        success:1,
-        image_url:`http://localhost:${process.env.PORT}/images/${req.file.filename}`
-    })
-})
+  const product = await Product.create({
+    id,
+    name,
+    image,
+    old_price,
+    new_price,
+    category,
+  });
 
+  console.log(product);
+  await product.save();
+  res.json({
+    success: 1,
+    name: req.body.name,
+  });
+});
 
 app.listen(process.env.PORT, (error) => {
   if (!error) {
---
+++ backend/models/product.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    new_price: {
+      type: Number,
+      required: true,
+    },
+    old_price: {
+      type: Number,
+      required: true,
+    },
+   
+    available: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
